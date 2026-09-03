cb22c46556224986a888266af7726336971f5dcf
deibysguerrero <deibysguerrero0@gmail.com>
2026-09-03 19:20:01 -0400

    Update WorldLevel.cpp
    Update

--- src/World/WorldLevel.cpp
+++ src/World/WorldLevel.cpp
@@ -1,22 +1,19 @@
+#include <Geode/Geode.hpp>
 #include <Geode/ui/Popup.hpp>
-#include <Geode/Geode.hpp>
 #include "WorldLevel.hpp"
-#include <Geode/Geode.hpp>
 
 using namespace geode::prelude;
-
 
 class WorldLevelPopup : public geode::Popup<std::string const&> {
 protected:
     bool setup(std::string const& value) override {
         auto winSize = CCDirector::sharedDirector()->getWinSize();
         this->setID("worldLevelPopup"_spr);
-    
         this->setTitle("");
 
         auto label = CCLabelBMFont::create(value.c_str(), "bigFont.fnt");
         label->setPosition(winSize / 2);
-        this->addChild(label);
+        this->m_mainLayer->addChild(label);
 
         return true;
     }
@@ -24,8 +21,6 @@ protected:
 public:
     static WorldLevelPopup* create(std::string const& text) {
         auto ret = new WorldLevelPopup();
-
-        
         if (ret && ret->initAnchored(300.f, 260.f, text)) {
             ret->autorelease();
             return ret;
