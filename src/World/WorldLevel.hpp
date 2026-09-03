69ffadec4174d89de729d1a69cdc2d2657a393f0
deibysguerrero <deibysguerrero0@gmail.com>
2026-09-03 19:22:51 -0400

    Update WorldLevel.hpp
    Idk

--- src/World/WorldLevel.hpp
+++ src/World/WorldLevel.hpp
@@ -3,23 +3,17 @@
 #include <Geode/Geode.hpp>
 #include <Geode/ui/Popup.hpp>
 
-class WorldLevel : public geode::Popup<std::string const&> 
-
+class WorldLevel : public geode::Popup<std::string const&> {
 protected:
-    bool init(std::string const& value);
+    bool setup(std::string const& value) override;
 
 public:
     static WorldLevel* create(std::string const& text);
     static cocos2d::CCScene* scene(std::string const& text);
 
 private:
-    
-
-
-  
-    virtual void keyBackClicked();
-    void updatePageWithObject(CCObject* page, CCObject* object);
-    void onClose(CCObject*);
+    virtual void keyBackClicked() override;
+    void updatePageWithObject(cocos2d::CCObject* page, cocos2d::CCObject* object);
+    void onClose(cocos2d::CCObject* sender);
     int m_level;
 };
-
